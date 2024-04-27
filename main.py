658784b83f206f4035a579d1bb25460149023e97
jagadesh2006 <166505888+jagadesh2006@users.noreply.github.com>
2024-04-27 18:12:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -22,16 +22,30 @@ start_buttons = InlineKeyboardMarkup(
 lang_buttons = InlineKeyboardMarkup(
     [[
         InlineKeyboardButton("Tamil",callback_data = "ta"),
-        InlineKeyboardButton("Japanese",callback_data ="ja)
+        InlineKeyboardButton("Japanese",callback_data ="ja"),
+        InlineKeyboardButton("Telugu",callback_data = "te")
+    ],[
+    	InlineKeyboardButton("✖️ Close",callback_data = "close")
     ]]
 )
 
 audio_buttons = InlineKeyboardMarkup(
     [[
-        InlineKeyboardButton("Audio",callback_data = "Audio"),
-        InlineKeyboardButton("Translated Audio",callback_data ="Audio")
+        InlineKeyboardButton(" 🎵 Audio",callback_data = "audio"),
+        InlineKeyboardButton("🎵 Translated Audio",callback_data ="tr_audio")
+    ],[
+    	InlineKeyboardButton("🔙 Back",callback_data = "lang"),
+    	InlineKeyboardButton("✖️ Close",callback_data = "close")
     ]]
 )
+
+close_buttons = InlineKeyboardMarkup(
+	[[
+		InlineKeyboardButton("✖️ Close",callback_data = "close")
+	]]
+)
+    
+
 @app.on_message(filters.command("start"))
 async def start(Client,message):
     await message.reply(
@@ -44,14 +58,42 @@ async def start(Client,message):
 async def lang(Client,message):
     await message.reply(
         "Choose Below Language to translate the text..",
-        reply_markup = lang_buttons
-    )
-    
+        quote= True,
+        reply_markup = lang_buttons,
+        )
+  
+@app.on_callback_query(filters.regex("lang"))
+async def lang(Client,message):
+    await message.edit_message_text(
+        "Choose Below Language to translate the text..",
+        reply_markup = lang_buttons,)
+    print(message)
+
+@app.on_callback_query(filters.regex("close"))
+async def close(client,message):
+      await message.message.delete()
+        		
+@app.on_callback_query(filters.regex("tr_audio"))
+async def tr_audio(client,message):
+	print(message)
+	tts = gTTS(tr_text.text,lang=language)
+	tts.save("tr_audio.mp3")
+	await message.message.reply_audio("tr_audio.mp3")     
+
+@app.on_callback_query(filters.regex("audio"))
+async def audio(client,message):
+	text = message.message.reply_to_message.text
+	tts = gTTS(text)
+	tts.save("audio.mp3")
+	await message.message.reply_audio("audio.mp3", reply_markup = close_buttons)     
+	   
 @app.on_callback_query()
 async def translate(Client,message):
+    global language
     language = message.data
+    global tr_text
     tr_text = translator.translate(message.message.reply_to_message.text,dest = language)
-    await message.message.reply(tr_text.text +"\nPronunciation :"+ tr_text.pronunciation,
+    await message.edit_message_text(tr_text.text +"\nPronunciation :"+ tr_text.pronunciation,
        reply_markup = audio_buttons
    )
                         
